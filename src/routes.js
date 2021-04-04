524f444f7050e88527f7755b6ac53a1daeb43ae1
Guilherme <guilherme.alves.campos@hotmail.com>
2021-04-04 20:11:55 -0300

    Refatorando Profile com data e update

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,47 @@ const routes = express.Router()
 
 const views = __dirname + "/views/"
 
-const profile = {
-    name: "Guilherme",
-    avatar: "https://avatars.githubusercontent.com/u/39139874?s=400&u=02cc7bf3fcdf420214e090723132653e1b7d7fe3&v=4",
-    "monthly-budget": 3000,
-    "days-per-week":5,
-    "hours-per-day":5,
-    "vacation-per-year":4,
-    "value-hour": 75
-}
+const Profile = {
+    data:{
+        
+            name: "Guilherme",
+            avatar: "https://avatars.githubusercontent.com/u/39139874?s=400&u=02cc7bf3fcdf420214e090723132653e1b7d7fe3&v=4",
+            "monthly-budget": 3000,
+            "days-per-week":5,
+            "hours-per-day":5,
+            "vacation-per-year":4,
+            "value-hour": 75
+        
+    },
+    controllers: {
+        index(req, res){
+            return res.render(views + "profile", {profile: Profile.data})
+        },
+
+    update(req, res){
+        // req.body para pegar os dados
+        const data = req.body
+        // definir quantas semanas tem num ano
+        const weeksPerYear = 52
+        // remover as semanas de férias do ano, para pegar quantas semanas tem num mes
+        const weeksPerMonth = (weeksPerYear - data["vacation-per-year"] ) / 12
+        // quantas horas por semana trabalhadas
+        const weekTotalHours = data["hours-per-day"] * data["days-per-week"]
+        // total de horas no mês
+        const monthlyTotalHours = weekTotalHours * weeksPerMonth 
+        // valor da hora
+        const valueHour = data["monthly-budget"] / monthlyTotalHours
+        
+        Profile.data = {
+         ...Profile.data, 
+         ...req.body,
+         "value-hour": valueHour
+           }
+          return res.redirect('/profile') 
+        }    
+    }   
+ }
+
 
 const Job = {
    data: [
@@ -36,7 +68,7 @@ const Job = {
 
     controllers: {
         index(req, res){
-            const updateJobs = Job.data.map((job) => {
+            const updatedJobs = Job.data.map((job) => {
                 //ajustes nos jobs
                 //calculo de temp restante
                    const remaining = Job.services.remainingDays(job)   
@@ -45,11 +77,11 @@ const Job = {
                        ...job,
                        remaining,
                        status,
-                       budget: profile["value-hour"]*job["total-hours"]
+                       budget: Profile.data["value-hour"]*job["total-hours"]
                } 
          })
     
-            res.render(views + "index",{profile,jobs: updateJobs})
+           return res.render(views + "index",{profile: Profile.data, jobs: updatedJobs })
       },
 
         create(req, res){
@@ -59,7 +91,7 @@ const Job = {
         save(req, res){
             const lastId = Job.data[Job.data.length - 1]?.id || 1;
 
-            jobs.push({
+            Jobs.data.push({
                 id: lastId + 1,
                 name: req.body.name,
                 "daily-hours": req.body["daily-hours"],
@@ -96,7 +128,7 @@ routes.get('/', Job.controllers.index)
 routes.get('/job',  Job.controllers.create)
 routes.post('/job', Job.controllers.save)
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
-routes.get('/profile', (req, res) => res.render(views + "profile", {profile}))
-    
+routes.get('/profile', Profile.controllers.index)
+routes.post('/profile', Profile.controllers.update)    
 
 module.exports = routes;
